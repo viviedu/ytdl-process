bba7f1cc584869e0d82217627b87412b1e53c4f4
keithp <keithp@vivi.io>
2022-07-26 14:16:44 +1000

    Change method of getting data in test file

--- ytdl-test.py
+++ ytdl-test.py
@@ -1,7 +1,7 @@
 import json
 import yt_dlp
 
-URL = 'https://www.youtube.com/playlist?list=PLQHHr8gPOsH7QTEzRAeBF1ivDAroEG4Iw'
+URL = 'https://www.youtube.com/watch?v=YqgcykDGB2A&t=10s'
 
 # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
 ydl_opts = {}
@@ -16,5 +16,14 @@ with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(URL, download=False)
 
     # ℹ️ ydl.sanitize_info makes the info json-serializable
-    with open("test_file.txt", 'w') as f:
+    with open("test_file2.txt", 'w') as f:
         f.write(json.dumps(ydl.sanitize_info(info)))
+
+"""
+ydl = yt_dlp.YoutubeDL(ydl_opts)
+info = ydl.extract_info(URL, download=False)
+
+# ℹ️ ydl.sanitize_info makes the info json-serializable
+with open("test_file3.txt", 'w') as f:
+    f.write(json.dumps(ydl.sanitize_info(info)))
+"""
