c915082b92886e0b29543d9984bdc2629aa6a57c
ryanchan799 <rchan168888@gmail.com>
2024-12-20 14:55:48 +1100

    Add change back

--- index.js
+++ index.js
@@ -269,10 +269,9 @@ module.exports.processV4 = (output, origin, locales = []) => {
     }
   }).filter(Boolean);
 
-  const desiredLocales = [["en-GB"], ["fr-FR"], ["pt-PT"]];
+  const desiredLocales = [['en-GB'], ['en-US'] ['fr-FR'], ['pt-PT'], ['de-DE']];
   const subtitleMap = {};
-  var subtitleFile;
-  var subtitleUrl;
+  var subtitleFile, subtitleUrl;
   for (const locale of desiredLocales) {
     subtitleFile = findBestSubtitleFile(subtitles, locale) || findBestSubtitleFile(automatic_captions, locale);
     subtitleUrl = subtitleFile ? `${origin}/ytdl/vtt?suburi=${encodeURIComponent(subtitleFile.subs.url)}` : '';
