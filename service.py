285b152355eee827bf02b13a10a988645301a3c2
Jasdeep Gill <jasdeepg1407@gmail.com>
2025-11-05 19:03:15 +1100

    Try simpler runtime arg

--- service.py
+++ service.py
@@ -46,7 +46,7 @@ class Handler(BaseHTTPRequestHandler):
 
         ydl_opts = {
             'forcejson': True,
-            'js_runtimes': { 'node': { 'config': {} } },
+            'js_runtimes': { 'node': {} },
             'logger': self,
             'quiet': True,
             'simulate': True
